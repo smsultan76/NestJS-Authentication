323783f7c5dd73e247d49e552e84c73c710443d9
SULTANUM MOBIN <sultanum.mobin@gmail.com>
2025-10-27 21:50:53 +0600

    chat module updated

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { OpenAIService } from './openai.service';
+import { ContentChat } from './entities/content-chat.entity';
+import { ContentMessage } from './entities/content-message.entity';
 
 @Module({
-  providers: [ChatService],
-  controllers: [ChatController]
+  imports: [
+    TypeOrmModule.forFeature([ContentChat, ContentMessage]),
+    HttpModule,
+  ],
+  controllers: [ChatController],
+  providers: [ChatService, OpenAIService],
+  exports: [ChatService],
 })
 export class ChatModule {}
